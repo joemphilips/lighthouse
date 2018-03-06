dc1249d614295b8631faf22abc775ebd49d48aad
joemphilips <joemphilips@gmail.com>
2018-03-06 13:39:17 +0900

    separete protoc as independent library

---
+++ packages/wallet-protoc/index.ts
@@ -0,0 +1,4 @@
+import { * as BackendServer} from "./generated/backendserver_pb_service"
+import { * as WalletServer} from "./generated/backendserver_pb_service"
+
+export {BackendServer, WalletServer};
